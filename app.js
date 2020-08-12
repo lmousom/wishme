8598395a6a46e3d962873146784532f986576bfb
Latiful Mousom <Latifulmousom@gmail.com>
2020-08-12 20:44:04 +0530

    Hands Down AI

---
+++ app.js
@@ -0,0 +1,64 @@
+const video = document.getElementById("camvideo");
+const audio = document.querySelector("#audio");
+let trackButton = document.getElementById("trackbutton");
+let updateNote = document.getElementById("updatenote");
+
+let isVideo = false;
+let model = null;
+
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video  
+    imageScaleFactor: 0.7,
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.75,    // confidence threshold for predictions.
+}
+
+function startVideo() {
+    handTrack.startVideo(video).then(function (status) {
+        console.log("video started", status);
+        if (status) {
+            updateNote.innerText = "Video started. Now tracking"
+            isVideo = true
+            runDetection()
+        } else {
+            updateNote.innerText = "Please enable video"
+        }
+    });
+}
+
+function toggleVideo() {
+    if (!isVideo) {
+        updateNote.innerText = "Starting video"
+        startVideo();
+    } else {
+        updateNote.innerText = "Stopping video"
+        handTrack.stopVideo(video)
+        isVideo = false;
+        updateNote.innerText = "Video stopped"
+    }
+}
+
+
+
+function runDetection() {
+    model.detect(video).then(predictions => {
+        if (isVideo) {
+            requestAnimationFrame(runDetection);
+        }
+        if (predictions.length > 0) {
+
+            audio.play();
+             
+            
+        }
+    });
+}
+
+// Load the model.
+handTrack.load(modelParams).then(lmodel => {
+
+    model = lmodel
+    updateNote.innerText = "Loaded Model!"
+    trackButton.disabled = false
+});
